1086456200a4b283e4ef6a7b56efc3bec1c2f69b
gon.lucero <gonzalom.lucero@gmail.com>
2021-12-21 17:59:49 -0300

    tiene que funcionar

--- api/src/db.js
+++ api/src/db.js
@@ -6,31 +6,39 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 //ACÁ ESTA TODA LA CONFIGURACIÓN DE LA DB
 let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
+  process.env.NODE_ENV === "production"?
+    // ? new Sequelize({
+    //     database: DB_NAME,
+    //     dialect: "postgres",
+    //     host: DB_HOST,
+    //     port: 5432,
+    //     username: DB_USER,
+    //     password: DB_PASSWORD,
+    //     pool: {
+    //       max: 3,
+    //       min: 1,
+    //       idle: 10000,
+    //     },
+    //     dialectOptions: {
+    //       ssl: {
+    //         require: true,
+    //         // Ref.: https://github.com/brianc/node-postgres/issues/2009
+    //         rejectUnauthorized: false,
+    //       },
+    //       keepAlive: true,
+    //     },
+    //     ssl: true,
+    //   })
+    new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ejmhibdimgcogg`,{
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
         { logging: false, native: false }
       );
 
