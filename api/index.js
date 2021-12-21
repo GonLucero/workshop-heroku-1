2d997e62128066c8a14021fb90145b4a52844f4f
gon.lucero <gonzalom.lucero@gmail.com>
2021-12-21 18:05:03 -0300

    funca

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => { // el force en true, resetea la base de datos todo el tiempo
+conn.sync({ force: false }).then(() => { // el force en true, resetea la base de datos todo el tiempo
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
