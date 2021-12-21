89a0625bae8f2716551ce6c54df1e1ad1253808d
gon.lucero <gonzalom.lucero@gmail.com>
2021-12-21 17:09:12 -0300

    api cambiada

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -1,24 +1,22 @@
-const { Product, conn } = require('../../src/db.js');
+const { Videogame, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('User model', () => {
+describe('Videogame model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
-    beforeEach(() => Product.sync({ force: true }));
+    beforeEach(() => Videogame.sync({ force: true }));
     describe('name', () => {
       it('should throw an error if name is null', (done) => {
-        Product.create({
-        })
+        Videogame.create({})
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
       it('should work when its a valid name', () => {
-        Product.create({ name: 'Producto' });
+        Recipe.create({ name: 'Super Mario Bros' });
       });
     });
   });
 });
-
--- api/tests/routes/videogame.spec.js
+++ api/tests/routes/videogame.spec.js
@@ -2,23 +2,23 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Product, conn } = require('../../src/db.js');
+const { Videogame, conn } = require('../../src/db.js');
 
 const agent = session(app);
-const product = {
-  name: 'producto',
+const videogame = {
+  name: 'Super Mario Bros',
 };
 
-describe('PRODUCT routes', () => {
+describe('Videogame routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Product.sync({ force: true })
-    .then(() => Product.create(product)));
-  describe('GET /products', () => {
-    it('should get 200', () => 
-      agent.get('/products/').expect(200)
+  beforeEach(() => Videogame.sync({ force: true })
+    .then(() => Videogame.create(videogame)));
+  describe('GET /videogames', () => {
+    it('should get 200', () =>
+      agent.get('/videogames').expect(200)
     );
   });
 });
