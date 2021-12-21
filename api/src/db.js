2c9cf136b01daac756e628956ee828d342327c4f
gon.lucero <gonzalom.lucero@gmail.com>
2021-12-21 17:47:39 -0300

    tiene que funcionar

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
         { logging: false, native: false }
       );
 
